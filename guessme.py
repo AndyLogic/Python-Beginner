8e2048e16572b0a9f458194eb430452d989ab917
AndyLogic <cristianvittoriocolonna@gmail.com>
2016-11-01 22:13:02 +1100

    Create guessme.py

---
+++ guessme.py
@@ -0,0 +1,39 @@
+import random
+
+def intro():
+    print("-----Welcome to the guessing game?-----")
+    print("-----Its guessing time-----")
+    play = ""
+    while play != "Yes" or "No":
+        play = input("Would you like to play the game? Please type Yes or No ")
+        if play == "Yes":
+            print("Let's Play")
+            break
+        elif play == "No":
+            print("Goodbye")
+            exit()
+        else:
+            print("What the heck did you type? Try again.")
+
+def guess():
+    guess = ""
+    rando = random.randint(1, 10)
+    while guess != rando:
+        guess = int(input('Please type your first guess : '))
+        if guess == rando:
+            print("Great work man!")
+        elif guess < rando:
+            print('Your Guess was too high please type your next guess')
+        elif guess > rando:
+            print('Your Guess was too low please type your next guess')
+        else:
+            print("Are you sure you are doing this righ!")
+
+
+
+def main():
+    intro()
+    guess()
+
+
+main()
